dedf0478462afd7b2513dc48cabdd4c8a01f495f
Jernej Vivod <je.vivod@gmail.com>
2020-01-13 13:47:05 +0100

    fix

--- evaluate.py
+++ evaluate.py
@@ -50,8 +50,8 @@ except:
     raise ValueError("Bad evaluation parameters")
 
 # Set CV parameters.
-N_SPLITS = 5
-N_REPEATS = 10
+N_SPLITS = 10
+N_REPEATS = 1
 
 # Set resampling method ('none' means no resampling).
 RESAMPLING_METHOD = 'random_oversampling'
